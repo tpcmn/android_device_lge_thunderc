4d94c3e8ef92e37ae1284ce5b7a3b46a3b340b64
thekraven <savethestyle1@gmail.com>
2012-02-16 21:40:20 -0600

    ics ls670 device folder
    Change-Id: I1b1817e08968e159db6c10d2e8cb968bcc43e22c

--- gadget_id/Android.mk
+++ gadget_id/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2010 Ricardo Cerqueira
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,13 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# This file is executed by build/envsetup.sh, and can use anything
-# defined in envsetup.sh.
-#
-# In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
 
-add_lunch_combo thunderc-eng
-add_lunch_combo thunderc-user
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),thunderc)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := gadget_id.c
+
+LOCAL_SHARED_LIBRARIES := libcutils
+
+LOCAL_MODULE := gadget_id
+
+include $(BUILD_EXECUTABLE)
+
+endif
