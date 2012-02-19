015d04232e7c8e66462f10bd86dec99289e1f0db
Mihai Costea <mihaia.andrei.costea@gmail.com>
2012-02-19 10:19:47 -0800

    We now use wpa_supplocant 6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ TARGET_PROVIDES_LIBRIL := true
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_CUSTOM_BRCM_PATCHRAM_PLUS := ../../../device/lge/p500/brcm_pachram_plus/brcm_patchram_plus.c
 
 # to enable the GPS HAL
 BOARD_USES_QCOM_GPS := true
@@ -65,7 +66,7 @@ WIFI_DRIVER_FW_AP_PATH          := "/system/etc/wl/rtecdc-apsta.bin"
 WIFI_DRIVER_MODULE_NAME         := "wireless"
 WIFI_DRIVER_MODULE_PATH         := "/system/lib/modules/wireless.ko"
 WIFI_DRIVER_MODULE_ARG          := "firmware_path=/etc/wl/rtecdc.bin nvram_path=/etc/wl/nvram.txt config_path=/data/misc/wifi/config"
-WPA_SUPPLICANT_VERSION          := VER_0_5_X
+WPA_SUPPLICANT_VERSION          := VER_0_6_X
 WIFI_DRIVER_HAS_LGE_SOFTAP      := true
 BOARD_WEXT_NO_COMBO_SCAN       := true
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
