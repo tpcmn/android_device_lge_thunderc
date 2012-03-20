e43fa22dae46ee105386f925ef3605cff4844e08
thekraven <savethestyle1@gmail.com>
2012-03-20 04:05:42 -0500

    add new cm cdma apn list
    Change-Id: I430894c623940c342e7d4acd07f1ce91492abc76

--- cm.mk
+++ cm.mk
@@ -3,6 +3,8 @@ PRODUCT_RELEASE_NAME := OptimusS
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
 
 # Inherit device configuration
 $(call inherit-product, device/lge/thunderc/full_thunderc.mk)
