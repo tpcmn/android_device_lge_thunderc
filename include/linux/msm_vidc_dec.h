d748d4577d005329d0dfdc6054b9a6e58eac3d4d
thekraven <savethestyle1@gmail.com>
2012-05-08 17:20:36 -0500

    might have broke omx building
    Change-Id: Id440c1e56b10f1f210e11dfb5b5f438559c5942f

--- include/linux/msm_vidc_dec.h
+++ include/linux/msm_vidc_dec.h
@@ -18,15 +18,12 @@
 /* Command called in invalid  state. */
 #define VDEC_S_EINVALSTATE	(VDEC_S_BASE + 4)
  /* Insufficient OS  resources - thread, memory etc. */
-
-
 #define VDEC_S_ENOSWRES	(VDEC_S_BASE + 5)
  /* Insufficient HW resources -  core capacity  maxed  out. */
 #define VDEC_S_ENOHWRES	(VDEC_S_BASE + 6)
 /* Invalid command  called */
 #define VDEC_S_EINVALCMD	(VDEC_S_BASE + 7)
 /* Command timeout. */
-
 #define VDEC_S_ETIMEOUT	(VDEC_S_BASE + 8)
 /* Pre-requirement is  not met for API. */
 #define VDEC_S_ENOPREREQ	(VDEC_S_BASE + 9)
@@ -37,11 +34,8 @@
 /* Command is not implemented by thedriver. */
 #define VDEC_S_ENOTIMPL	(VDEC_S_BASE + 12)
 /* Command is not implemented by the driver.  */
-
 #define VDEC_S_BUSY	(VDEC_S_BASE + 13)
 #define VDEC_S_INPUT_BITSTREAM_ERR (VDEC_S_BASE + 14)
-
-
 
 #define VDEC_INTF_VER	1
 #define VDEC_MSG_BASE	0x0000000
@@ -69,21 +63,6 @@
 #define VDEC_BUFFERFLAG_EOS	0x00000001
 #define VDEC_BUFFERFLAG_DECODEONLY	0x00000004
 #define VDEC_BUFFERFLAG_DATACORRUPT	0x00000008
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #define VDEC_BUFFERFLAG_ENDOFFRAME	0x00000010
 #define VDEC_BUFFERFLAG_SYNCFRAME	0x00000020
 #define VDEC_BUFFERFLAG_EXTRADATA	0x00000040
@@ -100,113 +79,7 @@
 #define VDEC_CMDBASE	0x800
 #define VDEC_CMD_SET_INTF_VERSION	(VDEC_CMDBASE)
 
-
 #define VDEC_IOCTL_MAGIC 'v'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 struct vdec_ioctl_msg {
 	void __user *in;
@@ -218,7 +91,6 @@ struct vdec_ioctl_msg {
 #define VDEC_IOCTL_GET_PROFILE_LEVEL_SUPPORTED \
 	_IOWR(VDEC_IOCTL_MAGIC, 0, struct vdec_ioctl_msg)
 
-
 /*CMD params:InputParam: NULL
   OutputParam: uint32_t(bitmask)*/
 #define VDEC_IOCTL_GET_INTERLACE_FORMAT \
@@ -226,7 +98,6 @@ struct vdec_ioctl_msg {
 
 /* CMD params: InputParam:  enum vdec_codec
    OutputParam: struct vdec_profile_level*/
-
 #define VDEC_IOCTL_GET_CURRENT_PROFILE_LEVEL \
 	_IOWR(VDEC_IOCTL_MAGIC, 2, struct vdec_ioctl_msg)
 
@@ -237,12 +108,8 @@ struct vdec_ioctl_msg {
 #define VDEC_IOCTL_GET_OUTPUT_FORMAT \
 	_IOWR(VDEC_IOCTL_MAGIC, 4, struct vdec_ioctl_msg)
 
-
-
 /*CMD params: SET: InputParam: enum vdec_codec OutputParam: NULL
   GET: InputParam: NULL OutputParam: enum vdec_codec*/
-
-
 #define VDEC_IOCTL_SET_CODEC \
 	_IOW(VDEC_IOCTL_MAGIC, 5, struct vdec_ioctl_msg)
 #define VDEC_IOCTL_GET_CODEC \
@@ -253,16 +120,12 @@ struct vdec_ioctl_msg {
 #define VDEC_IOCTL_SET_PICRES \
 	_IOW(VDEC_IOCTL_MAGIC, 7, struct vdec_ioctl_msg)
 #define VDEC_IOCTL_GET_PICRES \
-
-
 	_IOR(VDEC_IOCTL_MAGIC, 8, struct vdec_ioctl_msg)
 
 #define VDEC_IOCTL_SET_EXTRADATA \
 	_IOW(VDEC_IOCTL_MAGIC, 9, struct vdec_ioctl_msg)
-
 #define VDEC_IOCTL_GET_EXTRADATA \
 	_IOR(VDEC_IOCTL_MAGIC, 10, struct vdec_ioctl_msg)
-
 
 #define VDEC_IOCTL_SET_SEQUENCE_HEADER \
 	_IOW(VDEC_IOCTL_MAGIC, 11, struct vdec_ioctl_msg)
@@ -281,7 +144,6 @@ struct vdec_ioctl_msg {
 	_IOW(VDEC_IOCTL_MAGIC, 15, struct vdec_ioctl_msg)
 
 /*CMD params: CMD: InputParam - struct vdec_setbuffer_cmd, OutputParam - NULL*/
-
 #define VDEC_IOCTL_SET_BUFFER \
 	_IOW(VDEC_IOCTL_MAGIC, 16, struct vdec_ioctl_msg)
 
@@ -290,7 +152,6 @@ struct vdec_ioctl_msg {
 	_IOW(VDEC_IOCTL_MAGIC, 17, struct vdec_ioctl_msg)
 
 /*CMD params: InputParam - struct vdec_frameinfo , OutputParam - NULL*/
-
 #define VDEC_IOCTL_DECODE_FRAME \
 	_IOW(VDEC_IOCTL_MAGIC, 18, struct vdec_ioctl_msg)
 
@@ -300,9 +161,6 @@ struct vdec_ioctl_msg {
 #define VDEC_IOCTL_CMD_PAUSE _IO(VDEC_IOCTL_MAGIC, 22)
 #define VDEC_IOCTL_CMD_RESUME _IO(VDEC_IOCTL_MAGIC, 23)
 
-
-
-
 /*CMD params: InputParam - enum vdec_bufferflush , OutputParam - NULL */
 #define VDEC_IOCTL_CMD_FLUSH _IOW(VDEC_IOCTL_MAGIC, 24, struct vdec_ioctl_msg)
 
@@ -310,27 +168,17 @@ struct vdec_ioctl_msg {
  * IOCTL for getting asynchronous notification from driver
  * ========================================================*/
 
-
-
 /*IOCTL params: InputParam - NULL, OutputParam - struct vdec_msginfo*/
 #define VDEC_IOCTL_GET_NEXT_MSG \
 	_IOR(VDEC_IOCTL_MAGIC, 25, struct vdec_ioctl_msg)
 
 #define VDEC_IOCTL_STOP_NEXT_MSG _IO(VDEC_IOCTL_MAGIC, 26)
 
-
-
-
-
 #define VDEC_IOCTL_GET_NUMBER_INSTANCES \
 	_IOR(VDEC_IOCTL_MAGIC, 27, struct vdec_ioctl_msg)
 
-
-
 #define VDEC_IOCTL_SET_PICTURE_ORDER \
 	_IOW(VDEC_IOCTL_MAGIC, 28, struct vdec_ioctl_msg)
-
-
 
 #define VDEC_IOCTL_SET_FRAME_RATE \
 	_IOW(VDEC_IOCTL_MAGIC, 29, struct vdec_ioctl_msg)
@@ -338,21 +186,17 @@ struct vdec_ioctl_msg {
 #define VDEC_IOCTL_SET_H264_MV_BUFFER \
 	_IOW(VDEC_IOCTL_MAGIC, 30, struct vdec_ioctl_msg)
 
-
 #define VDEC_IOCTL_FREE_H264_MV_BUFFER \
 	_IOW(VDEC_IOCTL_MAGIC, 31, struct vdec_ioctl_msg)
 
-
 #define VDEC_IOCTL_GET_MV_BUFFER_SIZE  \
 	_IOR(VDEC_IOCTL_MAGIC, 32, struct vdec_ioctl_msg)
-
 
 #define VDEC_IOCTL_SET_IDR_ONLY_DECODING \
 	_IO(VDEC_IOCTL_MAGIC, 33)
 
 #define VDEC_IOCTL_SET_CONT_ON_RECONFIG  \
 	_IO(VDEC_IOCTL_MAGIC, 34)
-
 
 #define VDEC_IOCTL_SET_DISABLE_DMX \
 	_IOW(VDEC_IOCTL_MAGIC, 35, struct vdec_ioctl_msg)
@@ -378,7 +222,6 @@ enum vdec_buffer {
 	VDEC_BUFFER_TYPE_OUTPUT
 };
 
-
 struct vdec_allocatorproperty {
 	enum vdec_buffer buffer_type;
 	uint32_t mincount;
@@ -389,12 +232,6 @@ struct vdec_allocatorproperty {
 	uint32_t buf_poolid;
 };
 
-
-
-
-
-
-
 struct vdec_bufferpayload {
 	void __user *bufferaddr;
 	size_t buffer_len;
@@ -402,12 +239,6 @@ struct vdec_bufferpayload {
 	size_t offset;
 	size_t mmaped_size;
 };
-
-
-
-
-
-
 
 struct vdec_setbuffer_cmd {
 	enum vdec_buffer buffer_type;
@@ -418,12 +249,6 @@ struct vdec_fillbuffer_cmd {
 	struct vdec_bufferpayload buffer;
 	void *client_data;
 };
-
-
-
-
-
-
 
 enum vdec_bufferflush {
 	VDEC_FLUSH_TYPE_INPUT,
@@ -446,10 +271,6 @@ enum vdec_codec {
 	VDEC_CODECTYPE_VC1_RCV = 0xc
 };
 
-
-
-
-
 enum vdec_mpeg2_profile {
 	VDEC_MPEG2ProfileSimple = 0x1,
 	VDEC_MPEG2ProfileMain = 0x2,
@@ -462,15 +283,7 @@ enum vdec_mpeg2_profile {
 	VDEC_MPEG2ProfileMax = 0x7FFFFFFF
 };
 
-
-
 enum vdec_mpeg2_level {
-
-
-
-
-
-
 
 	VDEC_MPEG2LevelLL = 0x1,
 	VDEC_MPEG2LevelML = 0x2,
@@ -503,12 +316,6 @@ enum vdec_mpeg4_profile {
 	VDEC_MPEG4ProfileMax = 0x7FFFFFFF
 };
 
-
-
-
-
-
-
 enum vdec_mpeg4_level {
 	VDEC_MPEG4Level0 = 0x01,
 	VDEC_MPEG4Level0b = 0x02,
@@ -535,12 +342,6 @@ enum vdec_avc_profile {
 	VDEC_AVCProfileVendorStartUnused = 0x7F000000,
 	VDEC_AVCProfileMax = 0x7FFFFFFF
 };
-
-
-
-
-
-
 
 enum vdec_avc_level {
 	VDEC_AVCLevel1 = 0x01,
@@ -571,155 +372,12 @@ enum vdec_divx_profile {
 	VDEC_DIVXProfile_Handheld = 0x08,
 	VDEC_DIVXProfile_Portable = 0x10,
 	VDEC_DIVXProfile_HomeTheater = 0x20
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 };
 
 enum vdec_xvid_profile {
 	VDEC_XVIDProfile_Simple = 0x1,
 	VDEC_XVIDProfile_Advanced_Realtime_Simple = 0x2,
 	VDEC_XVIDProfile_Advanced_Simple = 0x4
-
-
-
-
-
-
-
-
 };
 
 enum vdec_xvid_level {
@@ -736,13 +394,6 @@ enum vdec_xvid_level {
 	VDEC_XVID_LEVEL_AS_L2 = 0x400,
 	VDEC_XVID_LEVEL_AS_L3 = 0x800,
 	VDEC_XVID_LEVEL_AS_L4 = 0x1000
-
-
-
-
-
-
-
 };
 
 enum vdec_h263profile {
@@ -758,14 +409,6 @@ enum vdec_h263profile {
 	VDEC_H263ProfileKhronosExtensions = 0x6F000000,
 	VDEC_H263ProfileVendorStartUnused = 0x7F000000,
 	VDEC_H263ProfileMax = 0x7FFFFFFF
-
-
-
-
-
-
-
-
 };
 
 enum vdec_h263level {
@@ -780,16 +423,6 @@ enum vdec_h263level {
 	VDEC_H263LevelKhronosExtensions = 0x6F000000,
 	VDEC_H263LevelVendorStartUnused = 0x7F000000,
 	VDEC_H263LevelMax = 0x7FFFFFFF
-
-
-
-
-
-
-
-
-
-
 };
 
 enum vdec_wmv_format {
@@ -800,20 +433,12 @@ enum vdec_wmv_format {
 	VDEC_WMFFormatKhronosExtensions = 0x6F000000,
 	VDEC_WMFFormatVendorStartUnused = 0x7F000000,
 	VDEC_WMVFormatMax = 0x7FFFFFFF
-
-
 };
-
-
-
 
 enum vdec_vc1_profile {
 	VDEC_VC1ProfileSimple = 0x1,
 	VDEC_VC1ProfileMain = 0x2,
 	VDEC_VC1ProfileAdvanced = 0x4
-
-
-
 };
 
 enum vdec_vc1_level {
@@ -827,57 +452,27 @@ enum vdec_vc1_level {
 	VDEC_VC1_LEVEL_A_L2 = 0x80,
 	VDEC_VC1_LEVEL_A_L3 = 0x100,
 	VDEC_VC1_LEVEL_A_L4 = 0x200
-
-
-
-
-
-
-
 };
-
-
-
-
-
 
 struct vdec_profile_level {
 	uint32_t profiles;
 	uint32_t levels;
-
-
-
-
-
-
-
-
 };
 
 enum vdec_interlaced_format {
 	VDEC_InterlaceFrameProgressive = 0x1,
 	VDEC_InterlaceInterleaveFrameTopFieldFirst = 0x2,
 	VDEC_InterlaceInterleaveFrameBottomFieldFirst = 0x4
-
-
-
 };
 
 enum vdec_output_fromat {
 	VDEC_YUV_FORMAT_NV12 = 0x1,
 	VDEC_YUV_FORMAT_TILE_4x2 = 0x2
-
-
-
-
 };
 
 enum vdec_output_order {
 	VDEC_ORDER_DISPLAY = 0x1,
 	VDEC_ORDER_DECODE = 0x2
-
-
-
 };
 
 struct vdec_picsize {
@@ -885,8 +480,6 @@ struct vdec_picsize {
 	uint32_t frame_height;
 	uint32_t stride;
 	uint32_t scan_lines;
-
-
 };
 
 struct vdec_seqheader {
@@ -894,13 +487,11 @@ struct vdec_seqheader {
 	size_t seq_header_len;
 	int pmem_fd;
 	size_t pmem_offset;
-
 };
 
 struct vdec_mberror {
 	void __user *ptr_errormap;
 	size_t err_mapsize;
-
 };
 
 struct vdec_input_frameinfo {
@@ -914,20 +505,13 @@ struct vdec_input_frameinfo {
 	size_t pmem_offset;
 	void __user *desc_addr;
 	uint32_t desc_size;
-
-
 };
-
-
-
-
 
 struct vdec_framesize {
 	uint32_t   left;
 	uint32_t   top;
 	uint32_t   right;
 	uint32_t   bottom;
-
 };
 
 struct vdec_output_frameinfo {
@@ -941,16 +525,11 @@ struct vdec_output_frameinfo {
 	void *input_frame_clientdata;
 	struct vdec_framesize framesize;
 	enum vdec_interlaced_format interlaced_format;
-
-
-
 };
 
 union vdec_msgdata {
 	struct vdec_output_frameinfo output_frame;
 	void *input_frame_clientdata;
-
-
 };
 
 struct vdec_msginfo {
@@ -958,21 +537,16 @@ struct vdec_msginfo {
 	uint32_t msgcode;
 	union vdec_msgdata msgdata;
 	size_t msgdatasize;
-
 };
 
 struct vdec_framerate {
 	unsigned long fps_denominator;
-
-
 	unsigned long fps_numerator;
 };
 
 struct vdec_h264_mv{
 	size_t size;
 	int count;
-
-
 	int pmem_fd;
 	int offset;
 };
