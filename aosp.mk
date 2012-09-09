840998495120a108b5139aae5d36baca30c4f86f
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-09-09 20:34:40 +0300

    Fix the makefile

--- aosp.mk
+++ aosp.mk
@@ -1,27 +1,19 @@
-# Boot Animation
-TARGET_SCREEN_HEIGHT := 480
-TARGET_SCREEN_WIDTH := 320
-
-# Include GSM stuff
+## Specify phone tech before including full_phone
 $(call inherit-product, vendor/common/config/gsm.mk)
 
-# Inherit device configuration for p500.
+PRODUCT_RELEASE_NAME := OptimusOne
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/common/config/common_full_phone.mk)
+
+# Inherit device configuration.
 $(call inherit-product, device/lge/p500/p500.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/common/config/common_mini_phone.mk)
 
 PRODUCT_NAME := aosp_p500
-PRODUCT_BRAND := lge
 PRODUCT_DEVICE := p500
+PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-P500
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_CHARACTERISTICS := phone
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=lge_p500 \
-    BUILD_FINGERPRINT="lge/thunderg/thunderg:2.3.3/GRI40/LG-P500-V20g.19C11F164C:user/release-keys" \
-    PRIVATE_BUILD_DESC="thunderg-user 2.3.3 GRI40 LG-P500-V20g.19C11F164C release-keys"
-
-# Release name and versioning
-PRODUCT_RELEASE_NAME := OptimusOne
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_p500 BUILD_ID=IMM76D BUILD_FINGERPRINT=lge/thunderg/thunderg:4.0.4/IMM76D/299849:user/release-keys PRIVATE_BUILD_DESC="thunderg-user 4.0.4 IMM76D 299849 release-keys" BUILD_NUMBER=299849
