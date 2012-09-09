cb2aae1af7d17c16f7b00591f5da28ac9de61feb
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-09-09 20:42:46 +0300

    Bad path

--- p500.mk
+++ p500.mk
@@ -1,7 +1,7 @@
 PRODUCT_AAPT_CONFIG := normal mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
-$(call inherit-product, device/p500/device.mk)
+$(call inherit-product, device/lge/p500/device.mk)
 $(call inherit-product-if-exists, vendor/lge/p500/p500-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/p500/overlay
