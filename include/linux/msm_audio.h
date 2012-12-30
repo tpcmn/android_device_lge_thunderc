4cb45d88b02c27afc01149206ae96f83a8e09939
thekraven <savethestyle1@gmail.com>
2012-12-30 08:22:39 -0600

    added omx headers and fix storage
    Change-Id: I88a7d487c2720ae2d0f8e5c5e5420741e3d94819

--- include/linux/msm_audio.h
+++ include/linux/msm_audio.h
@@ -240,6 +240,11 @@ struct msm_snd_endpoint {
 
 #define SND_AVC_CTL _IOW(SND_IOCTL_MAGIC, 6, unsigned *)
 #define SND_AGC_CTL _IOW(SND_IOCTL_MAGIC, 7, unsigned *)
+/* ZTE_Audio_CJ_100302, chenjun, 2010-3-2, start */
+/* ZTE_Audio-LB_CJ_1 chenjun 2009-10-28 start */
+#define SND_SET_AUDIO_LOOPBACK _IOW(SND_IOCTL_MAGIC, 8, unsigned *)
+/* ZTE_Audio-LB_CJ_1 chenjun 2009-10-28 end */
+/* ZTE_Audio_CJ_100302, chenjun, 2010-3-2, end */
 
 struct msm_audio_pcm_config {
 	uint32_t pcm_feedback;	/* 0 - disable > 0 - enable */
